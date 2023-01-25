f33aa45f7fd286323364c6d4476db6f39884f484
Skylar <skylar@anderson.is>
2023-01-24 22:01:16 -0800

    cleanup

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask_cors import CORS
-from langchain_bot_simple import get_answer
+from bot import get_answer
 
 app = Flask(__name__)
 CORS(app)
@@ -8,11 +8,7 @@ CORS(app)
 @app.route('/', methods=['GET'])
 def home():
     question = request.args.get('question')
-    print('question:')
-    print(question)
     if question:
       return get_answer(question)
     else:
       return "please provide a question!"
-
-# app.run()
--- bot.py
+++ bot.py
@@ -2,43 +2,17 @@ import openai
 from langchain.llms import OpenAI
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.docstore.document import Document
-import requests
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.faiss import FAISS
 from langchain.text_splitter import CharacterTextSplitter
-import pathlib
-import subprocess
-import tempfile
-
-
-
-def get_github_docs(repo_owner, repo_name):
-    with tempfile.TemporaryDirectory() as d:
-        subprocess.check_call(
-            f"git clone --depth 1 https://github.com/{repo_owner}/{repo_name}.git .",
-            cwd=d,
-            shell=True,
-        )
-        git_sha = (
-            subprocess.check_output("git rev-parse HEAD", shell=True, cwd=d)
-            .decode("utf-8")
-            .strip()
-        )
-        repo_path = pathlib.Path(d)
-        markdown_files = list(repo_path.glob("**/*.md")) + list(
-            repo_path.glob("**/*.mdx")
-        )
-        for markdown_file in markdown_files:
-            with open(markdown_file, "r") as f:
-                relative_path = markdown_file.relative_to(repo_path)
-                github_url = f"https://github.com/{repo_owner}/{repo_name}/blob/{git_sha}/{relative_path}"
-                yield Document(page_content=f.read(), metadata={"source": github_url})
-
+from get_github_docs import get_github_docs 
+from prompt import PROMPT
 
 sources = get_github_docs("primer", "design")
 
 source_chunks = []
 splitter = CharacterTextSplitter(separator=" ", chunk_size=1024, chunk_overlap=0)
+
 for source in sources:
     for chunk in splitter.split_text(source.page_content):
         source_chunks.append(Document(page_content=chunk, metadata=source.metadata))
@@ -49,15 +23,14 @@ search_index = FAISS.from_documents(source_chunks, OpenAIEmbeddings())
 # answer generation.  0 = no entropy.  1 = max entropy.
 # a higher temp will result in riskier and more creative
 # responses.
-temp = .75
+temp = .1
 
 # k refers to how many results should be returned from
 # vector DB when searching. Also known as a "k-nearest
 # neighbors" (KNN) search
 max_sources = 4
 
-chain = load_qa_with_sources_chain(OpenAI(temperature=temp))
-
+chain = load_qa_with_sources_chain(OpenAI(temperature=temp), chain_type="stuff", prompt=PROMPT)
 def get_answer(question):
     return chain(
         {
